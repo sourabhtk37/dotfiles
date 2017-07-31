63725e6dba9fdc094c80ed9f875ff717642bf27a
T K Sourabh <sourabhtk37@gmail.com>
2017-07-31 19:03:30 +0530

    dot files updated Jul 31 2017 dot files updated Jul 31 2017 dot files updated Jul 31 2017 dot files updated Jul 31 2017

---
+++ config.fish
@@ -0,0 +1,108 @@
+# ssh
+export SSH_KEY_PATH="~/.ssh/"
+
+set -g theme_short_path yes
+set -g theme_display_git yes 
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support no
+set -g theme_display_virtualenv yes
+set -g theme_display_user no 
+set -g theme_display_vi yes
+set -g theme_display_date yes
+set -g theme_display_cmd_duration no
+set -g theme_title_display_process yes
+set -g theme_title_display_path no 
+set -g theme_title_use_abbreviated_path yes
+set -g theme_date_format "+%H:%M"
+set -g theme_avoid_ambiguous_glyphs no
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user your_normal_user
+set -g theme_color_scheme dark
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 1
+
+
+
+#
+# Aliases
+# (sorted alphabetically)
+#
+# Set personal aliases. For a full list of active aliases, run `alias`.
+
+alias pmm="python manage.py makemigrations"
+alias pmmg="python manage.py migrate"
+alias pmr="python manage.py runserver"
+alias xo="xdg-open"
+alias tmux="tmux -u"
+alias update="sudo apt update"
+alias upgrade="sudo apt upgrade"
+alias install="sudo apt install"
+
+
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
+alias gbs='git bisect'
+
+alias gcb='git checkout -b'
+alias gcf='git config --list'
+alias gcl='git clone --recursive'
+alias gclean='git clean -fd'
+alias gcm='git checkout master'
+alias gcd='git checkout develop'
+alias gcm='git commit -m'
+alias gco='git checkout'
+
+alias gd='git diff'
+
+alias gf='git fetch'
+
+alias ghh='git help'
+
+alias gpl='git pull'
+alias glg='git log --stat'
+alias glgp='git log --stat -p'
+alias glgg='git log --graph'
+alias glgga='git log --graph --decorate --all'
+alias glo='git log --oneline --decorate'
+alias glol="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glola="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+
+alias gm='git merge'
+
+alias gp='git push'
+
+alias gr='git remote'
+alias gra='git remote add'
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grb='git rebase -i'
+alias grbm='git rebase master'
+alias grbs='git rebase --skip'
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
+alias grmv='git remote rename'
+alias grrm='git remote remove'
+alias grset='git remote set-url'
+alias grup='git remote update'
+alias grv='git remote -v'
+
+alias gsi='git submodule init'
+alias gss='git status -s'
+alias gst='git stash'
+alias gstd='git stash drop'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+alias gsts='git stash show --text'
+
+alias gts='git tag -s'
+alias gtv='git tag | sort -V'
+
+alias gplr='git pull --rebase -v'
+
+set pres /home/sourabhtk37/work/presentation/cmdl-parsing
--- dot_updater.sh
+++ dot_updater.sh
@@ -2,24 +2,27 @@
 # dot_updater.sh
 # runs every 15 days and copies dot-files and push them to git.
 
-var="cp /home/sourabh/."
+var="cp /home/sourabhtk37/."
 
 # change directory to a valid git folder
-chdir /home/sourabh/work/dotfiles
+chdir /home/sourabhtk37/work/dotfiles
 
 # copies dotfiles into the current directory
-for dot in "tmux.conf" "gitconfig" "zshrc" "vimrc" "bashrc" "i3_config"
+for dot in "gitconfig" "vimrc" "bashrc" "config/fish/config.fish" "config/i3/config"
 do
-	eval $var$dot /home/sourabh/work/dotfiles 
+	eval $var$dot /home/sourabhtk37/work/dotfiles 
 done
 
 # get to the dotfiles folder to reach a git 
 # directory else git commands wont work
-cd /home/sourabh/work/dotfiles
+cd /home/sourabhtk37/work/dotfiles
 
 # retrieves the current day,year for commit message
 commit_message="date | cut -d ' ' -f 2,3,6"
 echo "dot files updated $(eval $commit_message)" >> template.txt
+
+# adds ssh passphrase to ssh-agent
+ssh-add ~/.ssh/github_main
 
 # commits the changes
 git add .
@@ -29,9 +32,6 @@ git commit --file template.txt
 # be updated with new message as bash appends to
 # the end of the file
 rm template.txt
-
-# adds ssh passphrase to ssh-agent
-ssh-add ~/.ssh/github_main
 
 # via ssh so won't ask for authentication
 git push origin master
